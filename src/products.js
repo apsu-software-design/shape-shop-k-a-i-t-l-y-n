d4fc4964dd585c1043300d09b60a4d40ea498fc0
Kaitlyn <khardin9@my.apsu.edu>
2021-11-12 21:10:42 -0600

    update

--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Product = void 0;
+exports.ProductList = exports.Product = void 0;
 var Product = /** @class */ (function () {
     function Product(name, price, description) {
         this.name = name;
@@ -13,3 +13,23 @@ var Product = /** @class */ (function () {
     return Product;
 }());
 exports.Product = Product;
+//MODEL
+var ProductList = /** @class */ (function () {
+    function ProductList() {
+        this.productList = [];
+        this.quantityList = [];
+    }
+    ProductList.prototype.addProduct = function (product) {
+        this.productList.push(product);
+    };
+    ProductList.prototype.removeProduct = function (index) {
+        this.productList.splice(index, 1);
+    };
+    ProductList.prototype.getProductList = function () {
+        var productList = this.productList;
+        return productList;
+    };
+    return ProductList;
+}());
+exports.ProductList = ProductList;
+//VIEW
